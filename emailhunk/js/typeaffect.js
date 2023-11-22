a2f56d8ae0ea5a670523b57d6072d513b476f00e
helalmerchan <helalmerchan@gmail.com>
2023-11-22 21:04:25 +0600

    pricetablehove + typeaffect

---
+++ emailhunk/js/typeaffect.js
@@ -0,0 +1,44 @@
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  const phrasesA = ["agency", "ecommerce", "marketers", "Home services"];
+  const elemA = document.querySelector(".typeaffect");
+  
+  const phrasesB = ["HTML Email", "Newsletter", "Template", "HTML code"];
+  const elemB = document.querySelector(".typeaffect2");
+
+
+  let sleepTime = 100;
+  let curPhraseIndex = 0;  
+
+  const writeLoop = async (el, phrss) => {
+    while (true) {
+      let curWord = phrss[curPhraseIndex];
+
+      for (let i = 0; i < curWord.length; i++) {
+        el.innerText = curWord.substring(0, i + 1);
+        await sleep(sleepTime);
+      }
+
+      await sleep(sleepTime * 10);
+
+      for (let i = curWord.length; i > 0; i--) {
+        el.innerText = curWord.substring(0, i - 1);
+        await sleep(sleepTime);
+      }
+
+      await sleep(sleepTime * 5);
+
+      if (curPhraseIndex === phrss.length - 1) {
+        curPhraseIndex = 0;
+      } else {
+        curPhraseIndex++;
+      }
+    }
+  };
+
+  
+  writeLoop(elemA, phrasesA);
+
+  writeLoop(elemB, phrasesB);
